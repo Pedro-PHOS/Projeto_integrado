cb54e42e92efc51abc57a0370bfc630a5462b2fc
LucasHSC <101414256+LucasHSC@users.noreply.github.com>
2022-05-13 20:13:57 -0300

    Criação da Página sobre

---
+++ Pagina_Sobre/pagina_sobre.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/pagina_sobre.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="icon" href="../Home/assets/Backgorund/icon.jpg">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Infotech | </title>
+</head>
+
+<!--Cabe�alho do Site-->
+<!--Se��o na qual s�o armazenados os links de navega��o do site-->
+<body>
+    <header>
+        <div class="logo">
+            <img src="../Home/assets/Logo.png" alt="">
+        </div>
+
+        <div class="menu">
+            <li><a href="../Home/index.html">Home</a></li>
+            <li><a href="#">Cursos</a></li>
+            <li><a href="../Login_Cadastro/LoginRegistro.php">Login</a></li>
+        </div>
+    </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
