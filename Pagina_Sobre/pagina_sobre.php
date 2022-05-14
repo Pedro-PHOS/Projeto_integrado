c2c5580b6b3fdb4a74a1e2e74034d30efe628f0a
LucasHSC <101414256+LucasHSC@users.noreply.github.com>
2022-05-13 21:32:27 -0300

    alguns ajustes da pagina sobre

--- Pagina_Sobre/pagina_sobre.php
+++ Pagina_Sobre/pagina_sobre.php
@@ -6,12 +6,11 @@
     <link rel="stylesheet" type="text/css" href="css/pagina_sobre.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="icon" href="../Home/assets/Backgorund/icon.jpg">
+    <script src="https://kit.fontawesome.com/2bbeedebb6.js" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Infotech | </title>
 </head>
 
-<!--Cabe�alho do Site-->
-<!--Se��o na qual s�o armazenados os links de navega��o do site-->
 <body>
     <header>
         <div class="logo">
@@ -25,7 +24,50 @@
         </div>
     </header>
 
-
+    <main>
+        <div class="titulo-sobre">
+            <h1>
+                <span>Sobre a Infotech</span>
+            </h1>
+            <p class="sobre-text">
+                    A Infotech é uma empresa criada por cinco alunos do anhanguera que se iniciou como um projeto universitário no ano de 2020, mas com o grande crescimento da plataforma aonde veio a se torna está grande empresa que lida com a parte de influenciar e orientar tanto empresas como pessoa física, que está em busca de iniciar na área e para pessoas que já se encontram dentro da área de T.I.
+                    A plataforma é uma empresa online que disponibiliza cursos no seguimento e relacionados a área de T.I, aonde se encontra uma base que se-pode incluir os determinados cursos para que os clientes e organizações informe e instrua o colaborador ou cliente os cursos necessários para estar dentro da organização, e para informar melhor qual curso que haja a necessidade de ter para que possa entrar na área.
+                    Dentro da plataforma se encontra cursos que vão lidar tanto com a para ter mais acessos a cursos com certificados será cobrado um valor para a utilização.            
+                </p>
+        </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <div class="social-container">
+            <ul>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-youtube"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/projeto_infotech/" target="_blank">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
 
 
 
